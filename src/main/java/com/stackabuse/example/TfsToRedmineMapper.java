b235641781c4674d3d2978f64d1bde54dbd69914
Батманов Дмитрий Михайлович <dmbatman@mts.ru>
2019-02-12 11:34:41 +0300

    add activity

--- src/main/java/com/stackabuse/example/TfsToRedmineMapper.java
+++ src/main/java/com/stackabuse/example/TfsToRedmineMapper.java
@@ -10,27 +10,25 @@ import org.json.JSONObject;
 public class TfsToRedmineMapper implements Processor {
 
 	static final int REDMINE_CF_TFS_ID = 293;
+	static final int REDMINE_CF_ACTIVITY = 294;
 	static final int REDMINE_PROJECT_ID = 525;
 	static final String REDMINE_HOST = "http://10.0.63.81";
-	
+
 	private final static HashMap<String, Integer> REDMINE_TRACKER_ID = new HashMap<String, Integer>();
-	static
-	{
+	static {
 		REDMINE_TRACKER_ID.put("Задача", 6);
 		REDMINE_TRACKER_ID.put("Ошибка", 1);
 		REDMINE_TRACKER_ID.put("Элемент невыполненной работы по продукту", 7);
-		
+
 	}
-	
+
 	private final static HashMap<String, Integer> REDMINE_STATUD_ID = new HashMap<String, Integer>();
-	static
-	{
+	static {
 		REDMINE_STATUD_ID.put("Список задач", 1);
 		REDMINE_STATUD_ID.put("Выполняется", 2);
 		REDMINE_STATUD_ID.put("Решена", 3);
 		REDMINE_STATUD_ID.put("Удалено", 6);
 	}
-	
 
 	@Override
 	public void process(Exchange exchange) throws Exception {
@@ -44,14 +42,25 @@ public class TfsToRedmineMapper implements Processor {
 			String subject = obj.getJSONObject("resource").getJSONObject("fields").getString("System.Title");
 
 			// redmine custom fields
+			JSONArray custom_fields = new JSONArray();
+			
+				// Tfs id
 			int tfs_id = obj.getJSONObject("resource").getInt("id");
 			JSONObject cf_tfs_id = new JSONObject();
 
 			cf_tfs_id.put("value", tfs_id);
 			cf_tfs_id.put("id", REDMINE_CF_TFS_ID);
-
-			JSONArray custom_fields = new JSONArray();
 			custom_fields.put(cf_tfs_id);
+
+				// Activity
+			if (obj.getJSONObject("resource").getJSONObject("fields").has("Microsoft.VSTS.Common.Activity")) {
+				String activity = obj.getJSONObject("resource").getJSONObject("fields").getString("Microsoft.VSTS.Common.Activity");
+				JSONObject activity_json = new JSONObject();
+				activity_json.put("value", activity);
+				activity_json.put("id", REDMINE_CF_ACTIVITY);
+				custom_fields.put(activity_json);
+			}
+
 
 			// create json for redmine
 			JSONObject issue = new JSONObject();
@@ -59,22 +68,26 @@ public class TfsToRedmineMapper implements Processor {
 			issue.put("project_id", REDMINE_PROJECT_ID);
 			issue.put("subject", subject);
 			issue.put("custom_fields", custom_fields);
-			issue.put("tracker_id", REDMINE_TRACKER_ID.get(obj.getJSONObject("resource").getJSONObject("fields").getString("System.WorkItemType")));
-			issue.put("status_id", REDMINE_STATUD_ID.get(obj.getJSONObject("resource").getJSONObject("fields").getString("System.State")));
-			issue.put("priority_id", obj.getJSONObject("resource").getJSONObject("fields").getInt("Microsoft.VSTS.Common.Priority"));
-
+			issue.put("tracker_id", REDMINE_TRACKER_ID
+					.get(obj.getJSONObject("resource").getJSONObject("fields").getString("System.WorkItemType")));
+			issue.put("status_id", REDMINE_STATUD_ID
+					.get(obj.getJSONObject("resource").getJSONObject("fields").getString("System.State")));
+			issue.put("priority_id",
+					obj.getJSONObject("resource").getJSONObject("fields").getInt("Microsoft.VSTS.Common.Priority"));
+			// Description
 			if (obj.getJSONObject("resource").getJSONObject("fields").has("System.Description")) {
 				String description = obj.getJSONObject("resource").getJSONObject("fields")
 						.getString("System.Description");
 				issue.put("description", description);
 			}
-			
+			// Assigned_to
 			if (obj.getJSONObject("resource").getJSONObject("fields").has("System.AssignedTo")) {
-				String tfs_assigned_to = obj.getJSONObject("resource").getJSONObject("fields").getString("System.AssignedTo");
-				String login = tfs_assigned_to.substring(tfs_assigned_to.indexOf("\\") + 1, tfs_assigned_to.indexOf(">"));
-			//	issue.put("assigned_to_id", redmine_user_id);
+				String tfs_assigned_to = obj.getJSONObject("resource").getJSONObject("fields")
+						.getString("System.AssignedTo");
+				String login = tfs_assigned_to.substring(tfs_assigned_to.indexOf("\\") + 1,
+						tfs_assigned_to.indexOf(">"));
+				// issue.put("assigned_to_id", redmine_user_id);
 			}
-
 
 			String redmine_json = new JSONObject().put("issue", issue).toString();
 
@@ -85,8 +98,6 @@ public class TfsToRedmineMapper implements Processor {
 			exchange.getOut().setHeader("url", url_create);
 			exchange.getOut().setHeader(Exchange.HTTP_METHOD, "POST");
 		}
-		
 
-
-}
+	}
 }
